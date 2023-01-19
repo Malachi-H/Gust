7d833b4bce762492e8522c3c16a13333a05497cb
Malachi-H <67351403+Malachi-H@users.noreply.github.com>
2023-01-19 17:11:10 +1100

    More Playing Around
    Explored the 2D player controller and investigated how texturing and colouring works in Ursina.

--- Code/main.py
+++ Code/main.py
@@ -1,4 +1,28 @@
-from ursina.main import Ursina
-from ursina.prefabs.first_person_controller import FirstPersonController
+from ursina import *
 
 app = Ursina()
+window.title = "Gust"
+
+def update():
+    pass
+
+
+if __name__ == "__main__":
+    camera.orthographic = True
+    camera.fov = 10
+
+    EditorCamera()
+
+    ground = Entity(
+        model="cube",
+        origin_y=0.5,
+        scale=(2, 2, 1),
+        collider="box",
+        # red color
+        color=color.rgb(255, 0, 0),
+        position=(0, 0, 0),
+        texture="white_cube",
+        y=1,
+    )
+
+    app.run()

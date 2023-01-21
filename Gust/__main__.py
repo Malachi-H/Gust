0230c82c3cd1be55265a8a0313d92d2186037422
Malachi-H <67351403+Malachi-H@users.noreply.github.com>
2023-01-21 19:26:19 +1100

    Added Pause Functionality Also replaced wildcard import with more specific imports and altered the default window settings

--- Gust/__main__.py
+++ Gust/__main__.py
@@ -1,11 +1,36 @@
-from ursina import *
+from ursina import window
+from ursina import Ursina
+from ursina import camera
+from ursina import EditorCamera
+from ursina import Entity
+from ursina import color
+from ursina import held_keys
+from ursina import application
+from ursina.main import time
+
 
 app = Ursina()
 window.title = "Gust"
+window.borderless = False
+window.fps_counter.enabled = False
+window.exit_button.enabled = False
+
+
+def pause_handler_input(key):
+    # Toggle pause when escape key is pressed
+    if key == "escape":
+        application.paused = not application.paused
+
+
+pause_handler = Entity(
+    ignore_paused=True, input=pause_handler_input
+)  # used to receive input despite game being paused as 'pause_handler' ignores pauses
 
 
 def update():
-    pass
+    ground.rotation_x += 20 * time.dt
+    ground.rotation_y += 200 * time.dt
+    ground.rotation_z += 200 * time.dt
 
 
 if __name__ == "__main__":
